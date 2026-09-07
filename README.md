7eeee8c32d314e2d52d9d322368f4f5c8225324a
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-09-07 18:52:41 +0300

    test

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Dimitar Ivanov – Personal Portfolio 🚀
 
 Welcome to my personal portfolio website! This is a sleek, modern, single-page application (SPA) built using **HTML, CSS, JavaScript**, and **lit-html** to showcase my projects, education, and skills in a stylish and interactive way.
--- views/projects.js
+++ views/projects.js
@@ -10,8 +10,7 @@ function projectsTemplate() {
   return html`
     <h2>Projects</h2>
     <p style="white-space: nowrap">
-      This seems to be my only publicly published project, but don't worry, I'm
-      working on a few others..<br />
+      A short list of my (un)finished projects, on which I was/am working the last years:<br />
       You can follow my GitHub account for new projects and more:
       <a href="https://github.com/dimitarivanov10">@dimitarivanov10</a>
     </p>
